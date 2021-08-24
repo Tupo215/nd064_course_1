97523510c6d424e3c9706eeff10e108a67e4fb41
Tupo215 <87462858+Tupo215@users.noreply.github.com>
2021-08-24 18:44:44 +0200

    Add files via upload

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,7 +2,20 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+import sys
 
+# Set logger to handle STDOUT and STDERR
+stdout_handler = sys.stdout
+stderr_handler = sys.stderr
+handler = [stderr_handler, stdout_handler]
+
+
+# Setting up logging 
+logging.basicConfig(handlers=(logging.StreamHandler(stream=handler),logging.FileHandler('app.log')), 
+                    level=logging.DEBUG, 
+                    format=' %(levelname)s %(name)s%(asctime)s : %(message)s' 
+                    )
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
@@ -28,6 +41,7 @@ def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
+    app.logger.info("The Index page is retrieved")
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered 
@@ -36,13 +50,17 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('A non-existing article is accessed and a 404 page is returned')  
       return render_template('404.html'), 404
-    else:
-      return render_template('post.html', post=post)
+    else:  
+        app.logger.info("Article %s retrieved!",post_id)
+        return render_template('post.html', post=post)
+    
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info("The About Us page is retrieved")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -62,9 +80,33 @@ def create():
             connection.close()
 
             return redirect(url_for('index'))
-
+       
     return render_template('create.html')
+    app.logger.info("%s article is created", title) 
+# Define the healthcheck
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+        response = json.dumps({'result':'OK-health'}),
+        status = 200,
+        mimetype = 'application/json'
+        )
+    
+    app.logger.info("Healthz request successfull")
+    return response
+
+#Define the metrics
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+        response = json.dumps({'db_connection_count': 1,'post_count': 6}),
+        status = 200,
+        mimetype = 'application/json'
+        )
+    
+    app.logger.info("Metrics request successfull")
+    return response
 
 # start the application on port 3111
-if __name__ == "__main__":
+if __name__ == "__main__": 
    app.run(host='0.0.0.0', port='3111')
